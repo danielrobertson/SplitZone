db256c418d6c0f940976b01cb4c4c7cf7f5712e8
danielrobertson <danielrobertson733@gmail.com>
2016-06-24 14:57:13 -0500

    first draft regular expression

--- bot.js
+++ bot.js
@@ -1,19 +1,20 @@
 var Botkit = require('botkit');
 
 var controller = Botkit.slackbot({
-  debug: false
-  //include "log: false" to disable logging
-  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+    debug: false
+        //include "log: false" to disable logging
+        //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
 // connect the bot to a stream of messages
 controller.spawn({
-  token: process.env.SLACK_BOT_TOKEN,
+    token: process.env.SLACK_BOT_TOKEN,
 }).startRTM()
 
 // give the bot something to listen for.
-controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+var timeRegex = '([0-9]{1,2})\s?(:([0-5]?[0-9]))?((([p][m])|([a][m]))|(([P][M])|([A][M])))';
 
-  bot.reply(message,'Hello yourself.');
+controller.hears(timeRegex, ['ambient'], function(bot, message) {
+    bot.reply(message, 'Did someone say ' + message.match[0] + '?');
 
 });
