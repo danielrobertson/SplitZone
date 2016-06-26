adc5f648d79289866ba598984a6a13ea96d9fbee
danielrobertson <danielrobertson733@gmail.com>
2016-06-26 15:05:16 -0500

    some math, but it's wrong

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TimezonesBot
+# SplitZone
 A Slack bot that reduces communication friction in geographically distributed teams by converting times to relevant time zones based on the users in a particular Slack channel without being prompted.  
 
 This project is currently in development. 
--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ var timeRegex = '([0-9]{1,2})\s?(:([0-5]?[0-9]))?((([p][m])|([a][m]))|(([P][M])|
 
 controller.hears(timeRegex, ['ambient'], function(bot, message) {
 	console.log("bot matched time - " + message.match[0]); 
+	var date = new Date(); 
 
     // find the time zones of users in this channel 
     var channel = message.channel;
@@ -24,9 +25,16 @@ controller.hears(timeRegex, ['ambient'], function(bot, message) {
             bot.api.users.info({
                 user:memberId
             }, function(err, userResponse) {
-   				 bot.reply(message, 'Tz offset - ' + userResponse.user.tz_offset);
+				var username = userResponse.user.name; 
+        		var tzOffsetHours = userResponse.user.tz_offset / 3600;
+        		var botTzOffsetHours = date.getTimezoneOffset() / 60; 
+        		var totalOffsetHours = botTzOffsetHours + tzOffsetHours; 
+        		date.setHours(totalOffsetHours); 
+
+        		var msg = username + " " + String(date.getHours())+ ":" + String(date.getMinutes()); 
+        		bot.reply(message, msg); 
+            		
             });
         });
     });
-
 });
