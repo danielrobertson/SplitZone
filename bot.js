a4fcff083e34618c25996c55aacb5ea268b81d08
danielrobertson <danielrobertson733@gmail.com>
2016-06-26 20:15:11 -0500

    progress, still wrong math

--- bot.js
+++ bot.js
@@ -15,25 +15,32 @@ controller.spawn({
 var timeRegex = '([0-9]{1,2})\s?(:([0-5]?[0-9]))?((([p][m])|([a][m]))|(([P][M])|([A][M])))';
 
 controller.hears(timeRegex, ['ambient'], function(bot, message) {
-	console.log("bot matched time - " + message.match[0]); 
-	var date = new Date(); 
+    console.log("bot matched time - " + message.match[0]);
 
     // find the time zones of users in this channel 
     var channel = message.channel;
-    bot.api.channels.info({channel}, function(err, channelResponse) {
+    bot.api.channels.info({
+        channel
+    }, function(err, channelResponse) {
         channelResponse.channel.members.forEach(function(memberId) {
             bot.api.users.info({
-                user:memberId
+                user: memberId
             }, function(err, userResponse) {
-				var username = userResponse.user.name; 
-        		var tzOffsetHours = userResponse.user.tz_offset / 3600;
-        		var botTzOffsetHours = date.getTimezoneOffset() / 60; 
-        		var totalOffsetHours = botTzOffsetHours + tzOffsetHours; 
-        		date.setHours(totalOffsetHours); 
+                var username = userResponse.user.name;
+                console.log(username);
+                var date = new Date();
+                console.log("timezone offset " + (date.getTimezoneOffset() / 60));
+                console.log("tz offset " + userResponse.user.tz_offset);
 
-        		var msg = username + " " + String(date.getHours())+ ":" + String(date.getMinutes()); 
-        		bot.reply(message, msg); 
-            		
+                var offset = (date.getTimezoneOffset() / 60) + (userResponse.user.tz_offset / 3600);
+                console.log("date hours " + date.getHours());
+
+                console.log("offset " + offset);
+                date.setHours(date.getHours() + offset);
+
+                var msg = username + " " + String(date.getHours()) + ":" + String(date.getMinutes());
+                bot.reply(message, msg);
+
             });
         });
     });
