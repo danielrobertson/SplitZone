a9a9add6a9ad248ae629fde8e301cf98a95a4a30
danielrobertson <danielrobertson733@gmail.com>
2016-09-20 17:37:07 -0500

    correct offsets in military time

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ var Botkit = require('botkit');
 
 var controller = Botkit.slackbot({
     debug: false
-        //include "log: false" to disable logging
-        //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
 // connect the bot to a stream of messages
@@ -11,7 +9,7 @@ controller.spawn({
     token: process.env.SLACK_BOT_TOKEN,
 }).startRTM()
 
-// give the bot something to listen for.
+// listen for a time 
 var timeRegex = '([0-9]{1,2})\s?(:([0-5]?[0-9]))?((([p][m])|([a][m]))|(([P][M])|([A][M])))';
 
 controller.hears(timeRegex, ['ambient'], function(bot, message) {
@@ -19,29 +17,46 @@ controller.hears(timeRegex, ['ambient'], function(bot, message) {
 
     // find the time zones of users in this channel 
     var channel = message.channel;
-    bot.api.channels.info({
-        channel
-    }, function(err, channelResponse) {
+    bot.reply(message, "To clarify, that time translates too...");
+    bot.api.channels.info({channel}, function(err, channelResponse) {
         channelResponse.channel.members.forEach(function(memberId) {
-            bot.api.users.info({
-                user: memberId
-            }, function(err, userResponse) {
+            bot.api.users.info({user: memberId}, function(err, userResponse) {
                 var username = userResponse.user.name;
                 console.log(username);
-                var date = new Date();
-                console.log("timezone offset " + (date.getTimezoneOffset() / 60));
-                console.log("tz offset " + userResponse.user.tz_offset);
-
+                
+                var date = parseTime(message.match[0]); 
                 var offset = (date.getTimezoneOffset() / 60) + (userResponse.user.tz_offset / 3600);
-                console.log("date hours " + date.getHours());
-
-                console.log("offset " + offset);
                 date.setHours(date.getHours() + offset);
 
-                var msg = username + " " + String(date.getHours()) + ":" + String(date.getMinutes());
+                var msg = String(date.getHours());
+                if(date.getMinutes() !== 0) {
+                    msg +=":" + String(date.getMinutes())
+                }
+                msg += " for " + username + "\n"; 
+                
                 bot.reply(message, msg);
-
             });
         });
     });
 });
+
+function parseTime(time) {
+    var dt = new Date();
+
+    var time = time.match(/(\d+)(?::(\d\d))?\s*(p?)/i);
+    if (!time) {
+        return NaN;
+    }
+    var hours = parseInt(time[1], 10);
+    if (hours == 12 && !time[3]) {
+        hours = 0;
+    }
+    else {
+        hours += (hours < 12 && time[3]) ? 12 : 0;
+    }
+
+    dt.setHours(hours);
+    dt.setMinutes(parseInt(time[2], 10) || 0);
+    dt.setSeconds(0, 0);
+    return dt;
+}
